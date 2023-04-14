d9f55072ea27fa5f4c01a6be5bb167bb67f7364f
Kim Björnsen Åklint <kim.bjornsen@timeplan.se>
2023-04-14 14:11:02 +0200

    start of calendar

--- frontend/src/components/Calendar/Calendar.tsx
+++ frontend/src/components/Calendar/Calendar.tsx
@@ -1,8 +1,96 @@
-import React from 'react';
-import styles from './Calendar.module.css';
+import React, { useState } from 'react';
+import { dummyEvents } from './calendarData';
+import {
+  Container,
+  Typography,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+  Button,
+} from '@mui/material';
+
+type EventType = {
+  id: number;
+  title: string;
+  startDate: Date;
+  endDate: Date;
+};
 
 const Calendar: React.FC = () => {
-  return <div className={styles.calendar}>Calendar component</div>;
+  const [selectedEvent, setSelectedEvent] = useState<EventType | null>(null);
+  const [openDialog, setOpenDialog] = useState(false);
+
+  const handleClickOpen = (event: EventType) => {
+    setSelectedEvent(event);
+    setOpenDialog(true);
+  };
+
+  const handleClose = () => {
+    setOpenDialog(false);
+  };
+
+  return (
+    <Container>
+      <Typography variant="h4" component="h1" gutterBottom>
+        Calendar
+      </Typography>
+      <Paper>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Event</TableCell>
+              <TableCell>Date</TableCell>
+              <TableCell>Time</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {dummyEvents.map((event) => (
+              <TableRow
+                key={event.id}
+                hover
+                onClick={() => handleClickOpen(event)}
+                style={{ cursor: 'pointer' }}
+              >
+                <TableCell>{event.title}</TableCell>
+                <TableCell>{event.startDate.toLocaleDateString()}</TableCell>
+                <TableCell>
+                  {event.startDate.toLocaleTimeString()} -{' '}
+                  {event.endDate.toLocaleTimeString()}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Paper>
+      <Dialog open={openDialog} onClose={handleClose}>
+        {selectedEvent && (
+          <>
+            <DialogTitle>{selectedEvent.title}</DialogTitle>
+            <DialogContent>
+              <DialogContentText>
+                Date: {selectedEvent.startDate.toLocaleDateString()}
+              </DialogContentText>
+              <DialogContentText>
+                Time: {selectedEvent.startDate.toLocaleTimeString()} -{' '}
+                {selectedEvent.endDate.toLocaleTimeString()}
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleClose}>Close</Button>
+            </DialogActions>
+          </>
+        )}
+      </Dialog>
+    </Container>
+  );
 };
 
 export default Calendar;
---
+++ frontend/src/components/Calendar/calendarData.ts
@@ -0,0 +1,15 @@
+export const dummyEvents = [
+  {
+    id: 1,
+    title: 'Meeting with John',
+    startDate: new Date(2023, 3, 14, 10, 0),
+    endDate: new Date(2023, 3, 14, 11, 0),
+  },
+  {
+    id: 2,
+    title: 'Lunch with Sarah',
+    startDate: new Date(2023, 3, 15, 12, 30),
+    endDate: new Date(2023, 3, 15, 13, 30),
+  },
+  // ... more dummy events
+];
